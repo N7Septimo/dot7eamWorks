cea3be95ea9c153284cbe6b587dce934e2b79953
Rodolfo Ivan Bustamante <RBustamante@N7Infosec.com>
2026-01-02 02:32:26 -0700

    Simplify fetch function by removing intercept logic

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
 export default {
-  async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-
-    // Example intercept
-    if (url.pathname === "/resume" || url.pathname.startsWith("/resume/")) {
-      // Option A: serve a static response
-      return new Response("Resume here", { headers: { "content-type": "text/plain" } });
-
-      // Option B: forward to a different hostname (recommended) like a tunnel host
-      // return fetch("https://rbustamante.dot7eam.io" + url.pathname + url.search, request);
-    }
-
-    // Default: send to your normal origin (EC2) by passing through
+  async fetch(request) {
     return fetch(request);
   }
 };
