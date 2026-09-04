0f28e379c80d79364e351fd686b684541dd0a9b8
Rodolfo Ivan Bustamante <RBustamante@N7Infosec.com>
2026-09-03 19:43:19 -0700

    Add fetch function to handle HTML response
    Updated  Version upload failed. You attempted to upload a Service Worker syntax script to the version upload API, which only supports ES Modules. See https://developers.cloudflare.com/workers/reference/migrate-to-module-workers/ for migration instructions. [code: 10216]
    19:26:02.991

--- main.js
+++ main.js
@@ -508,3 +508,15 @@ const HTML = `<!doctype html>
   </script>
 </body>
 </html>`;
+
+export default {
+  async fetch(request, env, ctx) {
+    return new Response(HTML, {
+      status: 200,
+      headers: {
+        "Content-Type": "text/html;charset=UTF-8",
+        ...SECURITY_HEADERS,
+      },
+    });
+  },
+};
